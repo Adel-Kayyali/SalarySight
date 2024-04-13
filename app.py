bd198f6c791ccbfaa01b3ef69fb852ad9ee7d6f9
ADEL KAYYALI <102745376+Adel-Kayyali@users.noreply.github.com>
2024-04-13 13:47:56 +0300

    Updated application to use environment variables

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 from routes.about import about_blueprint
 from routes.home import home_blueprint
 
+load_dotenv()
+
 app = Flask(__name__)
+
+mongo_uri = os.getenv('MONGO_URI')
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(about_blueprint)
